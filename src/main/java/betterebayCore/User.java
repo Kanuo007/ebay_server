ea0fecfac2af2033a0e68ce877be9bb94f36ea62
dannilics2015 <LiDanni@Danni-Li.local>
2016-06-10 16:33:56 -0700

    betterebayCore

---
+++ src/main/java/betterebayCore/User.java
@@ -0,0 +1,89 @@
+package betterebayCore;
+
+import java.security.Principal;
+
+public class User implements Principal {
+  private String name;
+  private String password;
+  private String email;
+
+  public User(String name, String password, String email) {
+    this.name = name;
+    this.password = password;
+    this.email = email;
+  }
+
+  @Override
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getPassword() {
+    return this.password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getEmail() {
+    return this.email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = (prime * result) + ((this.email == null) ? 0 : this.email.hashCode());
+    result = (prime * result) + ((this.name == null) ? 0 : this.name.hashCode());
+    result = (prime * result) + ((this.password == null) ? 0 : this.password.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+    User other = (User) obj;
+    if (this.email == null) {
+      if (other.email != null) {
+        return false;
+      }
+    } else if (!this.email.equals(other.email)) {
+      return false;
+    }
+    if (this.name == null) {
+      if (other.name != null) {
+        return false;
+      }
+    } else if (!this.name.equals(other.name)) {
+      return false;
+    }
+    if (this.password == null) {
+      if (other.password != null) {
+        return false;
+      }
+    } else if (!this.password.equals(other.password)) {
+      return false;
+    }
+    return true;
+  }
+
+
+
+}
