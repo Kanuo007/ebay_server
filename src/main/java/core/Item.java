6fd7984c04cd584578c989fc71c192f6b68a1f1c
biaji814 <wenchen@Wens-MacBook-Air.local>
2016-06-21 20:41:06 -0700

    Item.java

---
+++ src/main/java/core/Item.java
@@ -0,0 +1,23 @@
+package betterebay Core;
+
+import java.security.Principal;
+
+public class Item  implements Principal {
+	private int IID;
+	private int UID;
+	private String name;
+	private String color;
+	Private int size;
+	private int stockAmount;	
+	private double basePrice;
+	private double curBidPrice;
+	private double soldPrice;
+	private int deliveryFee；
+	private String description;
+	private String feedback;
+
+public Item(String name){
+	name = this.name;
+}
+
+
