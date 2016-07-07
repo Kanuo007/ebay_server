ef14e1c4e9dd491a8cdfaedbec1e9f9ab0b06eff
wenchen <chenwenjiayou@gmial.com>
2016-07-07 15:56:30 -0700

    main

--- src/main/java/resource/SearchResource.java
+++ src/main/java/resource/SearchResource.java
@@ -1,13 +1,12 @@
 package resource;
 
-import java.util.LinkedList;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.slf4j.Logger;
@@ -15,9 +14,7 @@ import org.slf4j.LoggerFactory;
 
 import com.codahale.metrics.annotation.Timed;
 
-import api.Register;
 import core.Item;
-import core.User;
 import db.ItemDao;
 import io.dropwizard.hibernate.UnitOfWork;
 
@@ -26,20 +23,52 @@ import io.dropwizard.hibernate.UnitOfWork;
 public class SearchResource {
 
   private ItemDao itemDao;
+  private List<Item> res;
   private static Logger logger = LoggerFactory.getLogger(SearchResource.class);
+
 
   public SearchResource() {}
 
+
   @GET
   @Timed
+  @Path("/ALL")
   @UnitOfWork
   @Consumes(MediaType.APPLICATION_JSON)
-  public List<Item> Search(@QueryParam("item_name") String name) {
-    if (!this.itemDao.findItemByName(name).) {
-      return new LinkedList<>();
+  public List<Item> SearchAll() {
+    List<Item> pre = this.itemDao.findAllItem();
+    this.res = null;
+    if (pre != null) {
+      for (int i = 0; i < pre.size(); i++) {
+        if (pre.get(i).isStatus() == true) {
+          this.res.add(pre.get(i));
+        }
+      }
     }
-
+    return this.res;
   }
+
+
+
+  @GET
+  @Timed
+  @Path("/{item}")
+  @UnitOfWork
+  @Consumes(MediaType.APPLICATION_JSON)
+  public List<Item> Search(@PathParam("item") String item) {
+    List<Item> pre = this.itemDao.findItemByName(item);
+    this.res = null;
+    if (pre != null) {
+      for (int i = 0; i < pre.size(); i++) {
+        if (pre.get(i).isStatus() == true) {
+          this.res.add(pre.get(i));
+        }
+      }
+    }
+    return this.res;
+  }
+
+
 
 }
 
