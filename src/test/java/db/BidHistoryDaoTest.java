c5bb2733b2572cf3a6c241f40ab93325fc3f72ac
Li <yang.li3@husky.neu.edu>
2016-07-12 13:50:06 -0700

    add test for Register class

---
+++ src/test/java/db/BidHistoryDaoTest.java
@@ -0,0 +1,57 @@
+package db;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BidHistoryDaoTest {
+
+  @Before
+  public void setUp() throws Exception {}
+
+  @After
+  public void tearDown() throws Exception {}
+
+  @Test
+  public void testBidHistoryDao() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testFindBidHistoryByID() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testCreateBidHistory() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testFindAllBidHistory() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testFindByBidderId() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testFindByItemId() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testFindByTopPriceById() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testFindByBidTime() {
+    fail("Not yet implemented");
+  }
+
+}
---
+++ src/test/java/db/CreditCardDaoTest.java
@@ -0,0 +1,42 @@
+package db;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CreditCardDaoTest {
+
+  @Before
+  public void setUp() throws Exception {}
+
+  @After
+  public void tearDown() throws Exception {}
+
+  @Test
+  public void testCreditCardDao() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testFindCreditCardByID() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testCreateCreditCard() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testFindAllCreditCard() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testFindByUserId() {
+    fail("Not yet implemented");
+  }
+
+}
