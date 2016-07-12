347acb921bbc6a54a7018704e63ed403e6569ce8
Danni Li <dannylee8812@gmail.com>
2016-07-12 13:02:14 -0700

    added method in AuctionResource

--- src/main/java/resource/AuctionResource.java
+++ src/main/java/resource/AuctionResource.java
@@ -1,16 +1,17 @@
 package resource;
 
-import com.codahale.metrics.annotation.Timed;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.util.List;
+import java.util.Optional;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.codahale.metrics.annotation.Timed;
 
 import core.Item;
 import db.ItemDao;
@@ -32,5 +33,26 @@ public class AuctionResource {
   @UnitOfWork
   public List<Item> findAllAvailableItems() {
     return this.itemDao.findItemByAvailability();
+  }
+
+  @GET
+  @Timed
+  @UnitOfWork
+  public List<Item> findNamedItems(String name) {
+    return this.itemDao.findItemByName(name);
+  }
+
+  @GET
+  @Timed
+  @UnitOfWork
+  public List<Item> findNameColorSizeItems(String name, String color, int size) {
+    return this.itemDao.findItemByNameColorSize(name, color, size);
+  }
+
+  @GET
+  @Timed
+  @UnitOfWork
+  public Optional<Item> findIDItems(Long id) {
+    return this.itemDao.findItemByID(id);
   }
 }
