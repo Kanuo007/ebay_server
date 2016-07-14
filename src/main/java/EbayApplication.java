0f5e4c9cf507144c65c3b79d1b4b575a796f8c6a
Baoheng ling <Baoheng ling>
2016-07-13 22:07:10 -0700

    Procfile

--- src/main/java/EbayApplication.java
+++ src/main/java/EbayApplication.java
@@ -1,3 +1,5 @@
+import de.thomaskrille.dropwizard_template_config.TemplateConfigBundle;
+
 import core.Address;
 import core.BidHistory;
 import core.CreditCard;
@@ -53,6 +55,7 @@ public class EbayApplication extends Application<EbayApplicationConfiguration> {
     public void initialize(Bootstrap<EbayApplicationConfiguration> bootstrap) {
         bootstrap.addBundle(this.migrations);
         bootstrap.addBundle(this.hibernateBundle);
+        bootstrap.addBundle(new TemplateConfigBundle());
     }
 
     @Override
