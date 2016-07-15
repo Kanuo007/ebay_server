d8befe5198eca671d318363201a167a06797760c
Danni Li <dannylee8812@gmail.com>
2016-07-14 17:15:05 -0700

    fix user_resource

--- src/main/java/resource/UserResource.java
+++ src/main/java/resource/UserResource.java
@@ -1,16 +1,16 @@
 package resource;
 
-import com.codahale.metrics.annotation.Timed;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.codahale.metrics.annotation.Timed;
 
 import api.Register;
 import core.User;
@@ -22,39 +22,39 @@ import io.dropwizard.hibernate.UnitOfWork;
 @Path("/user")
 @Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
-    private UserDao userDao;
-    private static Logger logger = LoggerFactory.getLogger(UserResource.class);
+  private UserDao userDao;
+  private static Logger logger = LoggerFactory.getLogger(UserResource.class);
 
-    public UserResource(UserDao userDao) {
-        this.userDao = userDao;
-    }
+  public UserResource(UserDao userDao) {
+    this.userDao = userDao;
+  }
 
-    @GET
-    @Timed
-    @Path("/log_in")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public User login(@Auth User user) {
-        if (this.userDao.UserNamePasswordMatch(user.getUser_name(), user.getUser_password())) {
-            return user;
-        } else {
-            return new User("invalid user name", "invalid password", "invalid email");
-        }
+  @GET
+  @Timed
+  @Path("/log_in")
+  @Consumes(MediaType.APPLICATION_JSON)
+  public User login(@Auth User user) {
+    if (this.userDao.UserNamePasswordMatch(user.getName(), user.getUser_password())) {
+      return user;
+    } else {
+      return new User("invalid user name", "invalid password", "invalid email");
     }
+  }
 
-    @POST
-    @Path("/register")
-    @Timed
-    @UnitOfWork
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Register register(User user) {
-        Register r;
-        if (!this.userDao.findUserByName(user.getUser_name()).isPresent()) {
-            // If user doesn't exist
-            this.userDao.createUser(user);
-            r = new Register(user.getUser_name(), user.getUser_email(), user.getUser_password(), "Success");
-        } else {
-            r = new Register("", "", "", "Failure");
-        }
-        return r;
+  @POST
+  @Path("/register")
+  @Timed
+  @UnitOfWork
+  @Consumes(MediaType.APPLICATION_JSON)
+  public Register register(User user) {
+    Register r;
+    if (!this.userDao.findUserByName(user.getName()).isPresent()) {
+      // If user doesn't exist
+      this.userDao.createUser(user);
+      r = new Register(user.getName(), user.getUser_email(), user.getUser_password(), "Success");
+    } else {
+      r = new Register("", "", "", "Failure");
     }
+    return r;
+  }
 }
