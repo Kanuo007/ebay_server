c183775a7c00419e0a540a2b0bc97dd2baf813fb
biaji814 <wenchen@Wens-MacBook-Air.local>
2016-07-21 14:51:56 -0700

    mian/test

--- src/test/java/core/NotificationTest.java
+++ src/test/java/core/NotificationTest.java
@@ -1,57 +1,65 @@
 package core;
 
-import static org.junit.Assert.*;
-
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class NotificationTest {
+  private Notification a1;
+  private Notification a2;
+
 
   @Before
-  public void setUp() throws Exception {}
+  public void setUp() throws Exception {
+    this.a1 = new Notification(1, 11, "Bid ends");
+    this.a2 = new Notification(2, 22, "BBB");
+
+
+  }
 
   @After
   public void tearDown() throws Exception {}
 
   @Test
   public void testGetId() {
-    fail("Not yet implemented");
+    Assert.assertEquals(this.a1.getId(), 0);
   }
 
   @Test
   public void testGetUser_id() {
-    fail("Not yet implemented");
+    Assert.assertEquals(this.a1.getUser_id(), 1);
+    Assert.assertNotEquals(this.a1.getUser_id(), 2);
   }
 
   @Test
   public void testGetTransaction_id() {
-    fail("Not yet implemented");
+    Assert.assertEquals(this.a1.getTransaction_id(), 11);
+    Assert.assertNotEquals(this.a2.getTransaction_id(), 111);
   }
 
   @Test
   public void testGetContent() {
-    fail("Not yet implemented");
-  }
-
-  @Test
-  public void testSetId() {
-    fail("Not yet implemented");
+    Assert.assertEquals(this.a2.getContent(), "BBB");
+    Assert.assertNotEquals(this.a1.getTransaction_id(), "asdfad");
   }
 
   @Test
   public void testSetUser_id() {
-    fail("Not yet implemented");
+    this.a1.setUser_id(000);
+    Assert.assertEquals(this.a1.getUser_id(), 000);
   }
 
   @Test
   public void testSetTransaction_id() {
-    fail("Not yet implemented");
+    this.a1.setTransaction_id(123);
+    Assert.assertEquals(this.a1.getTransaction_id(), 123);
   }
 
   @Test
   public void testSetContent() {
-    fail("Not yet implemented");
+    this.a1.setContent("ddd");
+    Assert.assertEquals(this.a1.getContent(), "ddd");
   }
 
 }
